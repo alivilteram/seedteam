669f9cf4bee39a36ff4fd6939ca0729421549ffa
alivilteram <hackervatanbib@yahoo.com>
2016-03-14 14:48:36 +0000

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (teleseed)",
-	"^[#!/]([Tt]eleseed)"
+    "^[#!/]([Ss]tats) (vilter)",
+	"^[#!/]([Vv]ilter)"
     }, 
   run = run
 }
